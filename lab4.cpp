c1445c0a058a3b5538f851c219de2bfa3291fe8e
Angel Torres <ator89@outlook.com>
2019-05-10 14:13:44 -0600

    crear matriz

--- lab4.cpp
+++ lab4.cpp
@@ -10,9 +10,16 @@ void initMatriz(char**,int);
 //Imprimir Matriz
 void printMatriz(char**,int);
 //Liberar Matriz
-void liberarMatriz(char**,int);
+void liberarMatriz(char**&,int);
 
 int main(){
+  int size = -1;
+  char** matriz = NULL;
+
+  matriz = crearMatriz(10);
+  initMatriz(matriz,10);
+  printMatriz(matriz,10);
+  liberarMatriz(matriz,10);
 
   return 0;
 }
@@ -24,6 +31,34 @@ char** crearMatriz(int size){
   for(int i = 0; i < size; i++){
     matriz[i] = new char[size];
   }
+  return matriz;
 }
 
+//Inicializar matriz
+void initMatriz(char** matriz, int size){
+  for(int i = 0; i < size; i++){
+    for(int j = 0; j < size; j++){
+      matriz[i][j] = '.';
+    }
+  }
+}
 
+//Imprimir matriz
+void printMatriz(char** matriz, int size){
+  for(int i = 0; i < size; i++){
+    for(int j = 0; j < size; j++){
+      cout << " " << matriz[i][j];
+    }
+    cout << endl;
+  }  
+}
+
+//Liberar matriz
+void liberarMatriz(char**& matriz, int size){
+  for(int i = 0; i < size; i++){
+    delete[] matriz[i];
+    matriz[i] = NULL;
+  }
+  delete[] matriz;
+  matriz = NULL;
+}
--- makefile
+++
@@ -1,5 +0,0 @@
-main: lab4.o
-  g++ lab4.o -o main
-
-lab4.o: lab4.cpp
-  g++ -c lab4.cpp
